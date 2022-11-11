c3ac19e0a1150bf31321dc4c886103a19f4c374d
Emanuele Vicentini <emanuele.vicentini@gmail.com>
2022-11-11 10:17:56 +0100

    Explicitly ignoring RIPEMD during testing for the time being.

--- .ci_build_config.rb
+++ .ci_build_config.rb
@@ -1,7 +1,13 @@
 MRuby::Build.new do |conf|
-  toolchain :gcc
+  conf.toolchain
   conf.enable_debug
   conf.gembox 'default'
   conf.gem '.'
   conf.enable_test
+
+  # Because some algorithms have been moved to OpenSSL's legacy provider and
+  # mruby-digest is not ready for this.
+  conf.cc do | cc|
+    cc.defines << %w(OPENSSL_NO_RIPEMD OPENSSL_NO_RIPEMD160 OPENSSL_NO_RMD160)
+  end
 end
